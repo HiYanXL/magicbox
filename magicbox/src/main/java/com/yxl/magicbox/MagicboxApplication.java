f20fa62b7cc9016e0fb59a87de9fb27ca2337cbe
HiYanXL <2606469283@qq.com>
2020-08-08 17:15:20 +0800

    20200808

--- magicbox/src/main/java/com/yxl/magicbox/MagicboxApplication.java
+++ magicbox/src/main/java/com/yxl/magicbox/MagicboxApplication.java
@@ -2,8 +2,10 @@ package com.yxl.magicbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//去除数据源配置
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MagicboxApplication {
 
 	public static void main(String[] args) {
